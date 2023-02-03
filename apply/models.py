83751702514f5198e84fd4aa33248090f1dbce07
ljy2855 <ljy2855@naver.com>
2023-02-03 22:26:37 +0900

    feat: Add init models

--- apply/models.py
+++ apply/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class TermType(models.TextChoices):
+    SPRING = 'spring'
+    FALL = 'fall'
+
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=300)
+
+class Recruitment(models.Model):
+    year = models.PositiveSmallIntegerField()
+    term = models.CharField(max_length=10, choices=TermType)
+    is_open = models.BooleanField(default=True)
+
+class Resume(models.Model):
+    name = models.CharField(max_length=10)
+    secret = models.CharField(max_length=20)
+    
+
+
 # Create your models here.
