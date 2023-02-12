56fea94bc86944f2e92beeccb8c4cb02e272f3ee
ljy2855 <ljy2855@naver.com>
2023-02-12 21:20:30 +0900

    feat: Implement apply models

--- apply/models.py
+++ apply/models.py
@@ -1,22 +1,41 @@
 from django.db import models
 
+from user.models import Applicant
+
 class TermType(models.TextChoices):
     SPRING = 'spring'
     FALL = 'fall'
 
-class Question(models.Model):
-    question = models.CharField(max_length=100)
-    answer = models.CharField(max_length=300)
 
 class Recruitment(models.Model):
     year = models.PositiveSmallIntegerField()
-    term = models.CharField(max_length=10, choices=TermType)
-    is_open = models.BooleanField(default=True)
+    term = models.CharField(max_length=10, choices=TermType.choices)
+    is_open = models.BooleanField()
+    start_time = models.DateField()
+    document_deadline = models.DateField()
+    interview_start_time = models.DateField()
+    interview_end_time = models.DateField()
+    announce_time = models.DateField()
+    
+class InterViewTime(models.Model):
+    time = models.DateTimeField()
+    is_fixed = models.BooleanField(default=False)
 
 class Resume(models.Model):
-    name = models.CharField(max_length=10)
-    secret = models.CharField(max_length=20)
-    
+    applicant = models.OneToOneField(Applicant,on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    student_id = models.PositiveSmallIntegerField()
+    semester = models.PositiveSmallIntegerField()
+    #지원서 답변
+    introduce = models.TextField(default='')
+    motivate = models.TextField(default='')
+    to_do = models.TextField(default='')
+    etc = models.TextField(default='')
+
+    interview_time_choice = models.ManyToManyField(InterViewTime,related_name="interview_time")
+    interview_requirement = models.TextField(default='')
+
+
 
 
 # Create your models here.
--- apply/views.py
+++ apply/views.py
@@ -1,5 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
+from apply.models import Recruitment
 
-# Create your views here.
+#TODO
+@api_view(['GET'])
+def get_recuit_session():
+    session = get_object_or_404(Recruitment)
+
