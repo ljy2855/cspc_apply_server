f73eb32ac30987753ba9b7bc0c6d2af0f8e27ba6
ljy2855 <ljy2855@naver.com>
2023-02-13 15:13:44 +0900

    fix: change swagger url

--- cspc_web/urls.py
+++ cspc_web/urls.py
@@ -29,6 +29,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
 
     ),
+    url="http://api:8000",
     public=True,
     permission_classes=[permissions.AllowAny],
 )
