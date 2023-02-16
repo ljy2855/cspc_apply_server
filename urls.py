b5a61c6e3c3bc3fdecb373eee8ad4b65e57d1120
YSHyeonn <103395383+YSHyeonn@users.noreply.github.com>
2023-02-16 23:29:24 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from apply.views import *
-
-urlpatterns = [
-    path('recruit',get_recuit_session),
-    path('resume',api_resume),
-]
