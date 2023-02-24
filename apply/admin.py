b7602b43c54a2ad1280e3424251c3e1a9f9203ac
ljy2855 <ljy2855@naver.com>
2023-02-24 21:55:41 +0900

    feat: Add apply result api

--- apply/admin.py
+++ apply/admin.py
@@ -3,7 +3,7 @@ from apply.models import *
 import csv
 from django.http import HttpResponse
 from django.db.models import Count
-
+from django.shortcuts import get_object_or_404
 
 @admin.action(description='csv 파일 다운로드')
 def get_all_resume(self, request, queryset):
@@ -27,7 +27,7 @@ def set_interview_time(self, request, queryset): #self = resume model , queryset
     times = [queryset.time for queryset in InterviewTime.objects.all()]
     meta = self.model._meta
 
-    q = Resume.objects.annotate(c=Count('interview_time_choice')).order_by('c')
+    q = queryset.annotate(c=Count('interview_time_choice')).order_by('c')
 
     for i in q:
         for j in i.interview_time_choice.all():
@@ -43,13 +43,17 @@ def set_interview_time(self, request, queryset): #self = resume model , queryset
                     j.save()
                     times.remove(j.time)
                 break
-
-
+@admin.action(description="면접 장소 일괄 지정")
+def set_interview_place(self,request,queryset):
+    place = get_object_or_404(InterviewPlace)
+    for query in queryset:
+        query.interview_place = place
+        query.save()
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
     list_display = ('applicant', 'name', 'semester',
                     'phone', 'fixed_interview_time')
-    actions=[get_all_resume,set_interview_time]
+    actions=[get_all_resume,set_interview_time,set_interview_place]
 
 
     def get_ordering(self, request):
@@ -57,7 +61,7 @@ class ResumeAdmin(admin.ModelAdmin):
 
 
 
-
+admin.site.register(InterviewPlace)
 admin.site.register(Recruitment)
 admin.site.register(InterviewTime)
 
