0f3172a57a7cd2168176f8b7c4495c4709201a5e
ljy2855 <ljy2855@naver.com>
2023-02-26 01:16:32 +0900

    feat: Add optional field to post resume

--- apply/serializers.py
+++ apply/serializers.py
@@ -18,8 +18,21 @@ class ResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resume
         fields = ['interview_time_choice', 'name', 'semester', 'phone',
-                  'introduce', 'motivate', 'to_do', 'etc', 'interview_requirement', 'applicant', 'updated_at']
-    
+                  'introduce', 'motivate', 'to_do', 'etc', 'applicant', 'updated_at']
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('interview_requirement', None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
     # def update(self, instance, validated_data):
     #     nested_serializer = self.fields['interview_time_choice']
     #     nested_instance = instance.interview_time_choice
